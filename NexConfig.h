bc7da58aad9119ab2d4c850771872f8aa6994f29
Bruno Horta <brunohorta82@gmail.com>
2023-07-15 11:07:36 +0100

    Update NexConfig.h

--- NexConfig.h
+++ NexConfig.h
@@ -1,11 +1,11 @@
 /**
  * @file NexConfig.h
  *
- * Options for user can be found here. 
+ * Options for user can be found here.
  *
  * @author  Wu Pengfei (email:<pengfei.wu@itead.cc>)
  * @date    2015/8/13
- * @copyright 
+ * @copyright
  * Copyright (C) 2014-2015 ITEAD Intelligent Systems Co., Ltd. \n
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -16,35 +16,43 @@
 #define __NEXCONFIG_H__
 
 /**
- * @addtogroup Configuration 
- * @{ 
+ * @addtogroup Configuration
+ * @{
  */
 
-/** 
- * Define DEBUG_SERIAL_ENABLE to enable debug serial. 
- * Comment it to disable debug serial. 
+/**
+ * Define DEBUG_SERIAL_ENABLE to enable debug serial.
+ * Comment it to disable debug serial.
  */
 #define DEBUG_SERIAL_ENABLE
 
 /**
- * Define dbSerial for the output of debug messages. 
+ * Define dbSerial for the output of debug messages.
  */
-#define dbSerial Serial
+#define dbSerial Serial2
 
 /**
- * Define nexSerial for communicate with Nextion touch panel. 
+ * Define nexSerial for communicate with Nextion touch panel.
  */
-#define nexSerial Serial2
-
+#define nexSerial Serial
 
 #ifdef DEBUG_SERIAL_ENABLE
-#define dbSerialPrint(a)    dbSerial.print(a)
-#define dbSerialPrintln(a)  dbSerial.println(a)
-#define dbSerialBegin(a)    dbSerial.begin(a)
+#define dbSerialPrint(a) dbSerial.print(a)
+#define dbSerialPrintln(a) dbSerial.println(a)
+#define dbSerialBegin(a) dbSerial.begin(a)
 #else
-#define dbSerialPrint(a)    do{}while(0)
-#define dbSerialPrintln(a)  do{}while(0)
-#define dbSerialBegin(a)    do{}while(0)
+#define dbSerialPrint(a) \
+    do                   \
+    {                    \
+    } while (0)
+#define dbSerialPrintln(a) \
+    do                     \
+    {                      \
+    } while (0)
+#define dbSerialBegin(a) \
+    do                   \
+    {                    \
+    } while (0)
 #endif
 
 /**
